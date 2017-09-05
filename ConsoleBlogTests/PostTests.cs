c821dbfea0671a4861d1220acad2533aafed3fbd
Monaghar <Rmonaghan90@gmail.com>
2017-09-05 15:02:07 -0700

    Update PostTests.cs

--- ConsoleBlogTests/PostTests.cs
+++ ConsoleBlogTests/PostTests.cs
@@ -27,15 +27,61 @@ namespace CodeSchoolBlog.Tests
         [TestMethod()]
         public void PostTestNullAuthorConstructor()
         {
-            Author testAuthor = new Author("Geoff", "Geoff@.Com", "total noob", "12334");
+            Author testAuthor = null;
             string testTitle = "my test title";
             string testBody = "alot of great info";
 
-            var testPost = new Post(testAuthor, testTitle, testBody);
-
-            Assert.AreEqual(testTitle, testPost.Title);
-            Assert.AreEqual(testBody, testPost.Body);
+            var testPost; 
+            
+            try
+            {
+               testPost = new Post(testAuthor, testTitle, testBody);
+               Assert.IsNull(testPost.Owner);
+            }
+            catch(ArgumentNullException)
+            {
+                Assert.Pass("Pest Constructor Author nulll test passed");
+            }
         }
 
+        [TestMethod()]
+        public void PostTestNullTitleConstructor()
+        {
+            Author testAuthor = new Author("Geoff", "Geoff@.Com", "total noob", "12334");
+            string testTitle = null;
+            string testBody = "alot of great info";
+
+            var testPost; 
+            
+            try
+            {
+               testPost = new Post(testAuthor, testTitle, testBody);
+               Assert.IsNull(testPost.Title);
+            }
+            catch(ArgumentNullException)
+            {
+                Assert.Pass("Post Constructor Title nulll test passed");
+            }
+        }
+        
+        [TestMethod()]
+        public void PostTestNullBodyConstructor()
+        {
+            Author testAuthor = new Author("Geoff", "Geoff@.Com", "total noob", "12334");
+            string testTitle = "my test title";
+            string testBody = null;
+
+            var testPost; 
+            
+            try
+            {
+               testPost = new Post(testAuthor, testTitle, testBody);
+               Assert.IsNull(testPost.Body);
+            }
+            catch(ArgumentNullException)
+            {
+                Assert.Pass("Post Constructor Body nulll test passed");
+            }
+        }
     }
 }
