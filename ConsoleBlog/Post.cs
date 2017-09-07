3726de370ef6feb3fbf6075a35c065b52fd8c06e
Monaghar <Rmonaghan90@gmail.com>
2017-09-07 08:56:09 -0700

    Update Post.cs

--- ConsoleBlog/Post.cs
+++ ConsoleBlog/Post.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CodeSchoolBlog
 {
-    public class Post : IPost
+    public class Post : IPost, IComment
     {
         public Author Owner { get; set; }
         public string Body { get; set; }
