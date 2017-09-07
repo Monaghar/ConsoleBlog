e7519b6b396f095a1ebc9d73211cc747a5ffbbf7
Monaghar <Rmonaghan90@gmail.com>
2017-09-07 10:04:22 -0700

    Update Comment.cs

--- ConsoleBlog/Comment.cs
+++ ConsoleBlog/Comment.cs
@@ -10,19 +10,20 @@ namespace CodeSchoolBlog
     {
         public string Body { get; set; }
         public DateTime dateTime { get; set; }
-        public Author Owner { get; set; }
+        //public Author Owner { get; set; }
+        public List<Comment> {get; set;}
 
         public Comment() { }
 
-        public Comment(string body, Author owner, Post post)
+        public Comment(string body)
         {
-            if(body != null && owner != null && post != null)
+            if(body != null)
             {
                 Body = body;
-                Owner = owner;
                 this.dateTime = DateTime.Now;
-                owner.MyComments.Add(this);
-                post.Comments.Add(this);
+                //Owner = owner;
+                //owner.MyComments.Add(this);
+                //post.Comments.Add(this);
             }
             else{throw new ArgumentNullException("Yo your Comment params are null fix em");}
         }
