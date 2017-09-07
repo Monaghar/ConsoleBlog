f1b8e9199c19b74c946670fc5a307a8ebc628689
Monaghar <Rmonaghan90@gmail.com>
2017-09-07 10:13:21 -0700

    Update Post.cs

--- ConsoleBlog/Post.cs
+++ ConsoleBlog/Post.cs
@@ -8,20 +8,18 @@ namespace CodeSchoolBlog
 {
     public class Post : IPost, IComment
     {
-        public Author Owner { get; set; }
         public string Body { get; set; }
         public DateTime dateTime { get; set; }
         public List<Comment> Comments { get; set; }
         public string Title { get; set; }
 
-        public Post(Author owner, string title, string body)
+        public Post(string title, string body)
         {
              if(owner != null && title != null && body != null)
             {
                 Title = title;
                 Body = body;
                 this.dateTime = DateTime.Now;
-                owner.MyPosts.Add(this);
                 Comments = new List<Comment>();
             }
             else {throw new ArgumentNullException("Yo your Post params are null fix em");} 
