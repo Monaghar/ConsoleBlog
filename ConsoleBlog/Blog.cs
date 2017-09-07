918f79f7f7e1817ffaadeeea0ec6bf69b528f35d
Monaghar <Rmonaghan90@gmail.com>
2017-09-07 13:08:10 -0700

    Update Blog.cs

--- ConsoleBlog/Blog.cs
+++ ConsoleBlog/Blog.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace CodeSchoolBlog
 {
-    public class Blog : IBlog
+    public static class Blog : IBlog
     {
-        public Author Owner { get; set; }
-        public List<Post> Posts { get; set; }
+        public static Author Owner { get; set; }
+        public static List<Post> Posts { get; set; }
 
-        public string Description { get; set; }
-        public string Title { get; set; }
+        public static string Description { get; set; }
+        public static string Title { get; set; }
 
         public Blog(Author owner, string title, string description)
         {
