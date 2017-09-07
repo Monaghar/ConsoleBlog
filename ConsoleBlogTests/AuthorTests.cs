7c15d1b1dc39155e59967d15f2a9df71e795b0ce
Monaghar <Rmonaghan90@gmail.com>
2017-09-07 13:52:52 -0700

    Update AuthorTests.cs

--- ConsoleBlogTests/AuthorTests.cs
+++ ConsoleBlogTests/AuthorTests.cs
@@ -104,5 +104,66 @@ namespace CodeSchool.Tests
             author.LogOut();
             Assert.AreEqual(false, author.IsLoggedIn);
         }
+        
+         [TestMethod()]
+        public void CreatePostTest()
+        {
+            string name = "Hodor";
+            string eMail = "HOR@DOR";
+            string bio = "I got turned simple by my time traveling master";
+            string password = "432221";
+
+            var author = new Author(name, eMail, bio, password);
+            
+            string title = "this is a test post";
+            string body = "so much testing";
+
+            author.LogIn("432221");
+            author.CreatePost(title, body);
+            
+            Assert.IsNotNull(Blog.Posts[0]);
+            Assert.IsNotNull(author.PostHistory[0]);
+        }
+        
+        [TestMethod()]
+        public void CreateCommentUnderPostTest()
+        {
+            string name = "Hodor";
+            string eMail = "HOR@DOR";
+            string bio = "I got turned simple by my time traveling master";
+            string password = "432221";
+
+            var author = new Author(name, eMail, bio, password);
+            string body = "so much testing";
+            string title = "test title";
+            string body2 = "Gee a lot of testing indeed");
+            
+            var post = new Post(title, body);
+            author.LogIn("432221");
+            
+            author.CreateComment(post, body2);
+            
+            Assert.IsNotNull(author.CommentHistory[0]);
+            Assert.IsNotNull(post.Comments[0]);
+        }
+        
+        [TestMethod()]
+        public void CreateCommentUnderCommentTest()
+        {
+            string name = "Hodor";
+            string eMail = "HOR@DOR";
+            string bio = "I got turned simple by my time traveling master";
+            string password = "432221";
+
+            var author = new Author(name, eMail, bio, password);
+            var comment = new Comment("this is a test comment");
+            string body = "so much testing";
+
+            author.LogIn("432221");
+            author.CreateComment(comment, body);
+            
+            Assert.IsNotNull(author.CommentHistory[0]);
+            Assert.IsNotNull(comment.CommentChain[0]);
+        }
     }
 }
