0561091d69a42e22eddb4e0c318f7706b3c585a6
Monaghar <Rmonaghan90@gmail.com>
2017-09-08 13:39:11 -0700

    Update Comment.cs

--- ConsoleBlog/Comment.cs
+++ ConsoleBlog/Comment.cs
@@ -17,10 +17,21 @@ namespace CodeSchoolBlog
 
         public Comment(string body, string signature)
         {
-            if(!string.IsNullOrEmpty(body))
+            if(!string.IsNullOrEmpty(body) && !string.IsNullOrEmpty(signature))
             {
                 Body = body;
                 SignatureBlock = signature;
+                this.dateTime = DateTime.Now;
+                CommentChain = new List<Comment>();
+            }
+            else{throw new ArgumentNullException("Yo your Comment params are null fix em");}
+        }
+        
+         public Comment(string body)
+        {
+            if(!string.IsNullOrEmpty(body))
+            {
+                Body = body;
                 this.dateTime = DateTime.Now;
                 CommentChain = new List<Comment>();
             }
