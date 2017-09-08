34d0ec074f031338611c12ba273aa4c8876ed3c4
Monaghar <Rmonaghan90@gmail.com>
2017-09-08 13:25:03 -0700

    Update IComment.cs

--- ConsoleBlog/IComment.cs
+++ ConsoleBlog/IComment.cs
@@ -5,6 +5,7 @@ namespace CodeSchoolBlog
     interface IComment : IOwner
     {
         string Body { get; set; }
+        string SignatureBlock {get; set;}
         DateTime dateTime { get; set; }
         List<Comment> {get: set:}
     }
