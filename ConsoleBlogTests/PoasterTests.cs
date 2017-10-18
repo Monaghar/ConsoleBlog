479bed7d339263a5236075ef59853b41e83ae6fc
Monaghar <Rmonaghan90@gmail.com>
2017-10-18 12:02:12 -0700

    Update PoasterTests.cs

--- ConsoleBlogTests/PoasterTests.cs
+++ ConsoleBlogTests/PoasterTests.cs
@@ -49,43 +49,43 @@ namespace CodeSchoolBlog.Tests
             Assert.IsNotNull(comment.CommentChain[0]);
         }
 
-        //[TestMethod()]
-        //public void CreateCommentWithQuoteUnderCommentTest()
-        //{
-        //    string name = "Hodor";
-        //    string eMail = "HOR@DOR";
-        //    string bio = "I got turned simple by my time traveling master";
-        //    string quote = "look at my quote!";
-        //    string password = "432221";
+        [TestMethod()]
+        public void CreateCommentWithQuoteUnderCommentTest()
+        {
+            string name = "Hodor";
+            string eMail = "HOR@DOR";
+            string bio = "I got turned simple by my time traveling master";
+            string quote = "look at my quote!";
+            string password = "432221";
 
-        //    var author = new Author(name, eMail, bio, password);
-        //    var comment = new Comment("this is a test comment");
-        //    string body = "so much testing";
+            var author = new Author(name, eMail, bio, password);
+            var comment = new Comment("this is a test comment");
+            string body = "so much testing";
 
-        //    author.LogIn("432221");
-        //    Poaster.CreateComment(author, comment, body, quote);
+            author.LogIn("432221");
+            Poaster.CreateComment(author, comment, body, quote);
 
-        //    Assert.AreEqual(comment.Body, "\"look at my quote!\" so much testing");
-        //}
+            Assert.AreEqual(comment.Body, "\"look at my quote!\" so much testing");
+        }
 
 
-        // [TestMethod()]
-        //public void CreateCommentWithQuoteUnderPostTest()
-        //{
-        //    string name = "Hodor";
-        //    string eMail = "HOR@DOR";
-        //    string bio = "I got turned simple by my time traveling master";
-        //    string quote = "look at my quote!";
-        //    string password = "432221";
+         [TestMethod()]
+        public void CreateCommentWithQuoteUnderPostTest()
+        {
+            string name = "Hodor";
+            string eMail = "HOR@DOR";
+            string bio = "I got turned simple by my time traveling master";
+            string quote = "look at my quote!";
+            string password = "432221";
 
-        //    var author = new Author(name, eMail, bio, password);
-        //    var post = new Post("this is a test post", "OOooo lookit dat body");
-        //    string body = "so much testing";
+            var author = new Author(name, eMail, bio, password);
+            var post = new Post("this is a test post", "OOooo lookit dat body");
+            string body = "so much testing";
 
-        //    author.LogIn("432221");
-        //    Poaster.CreateComment(author, post, body, quote);
+            author.LogIn("432221");
+            Poaster.CreateComment(author, post, body, quote);
 
-        //    Assert.AreEqual(comment.Body, "\"look at my quote!\" so much testing");
-        //}
+            Assert.AreEqual(comment.Body, "\"look at my quote!\" so much testing");
+        }
     }
 }
